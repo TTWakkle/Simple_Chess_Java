5dcfb0188851a7f54fe3c99831bb4cf1d9a587f9
MatthewSaccone49 <matthewsaccone9@gmail.com>
2025-02-14 22:49:39 -0500

    chess board

--- main/Src/ChessBoard.java
+++ main/Src/ChessBoard.java
@@ -1,2 +1,14 @@
 package main.Src;
-// hello matthew
+
+import java.awt.*;
+import javax.swing.*;
+
+public class ChessBoard {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Chess");
+		frame.setSize(800, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		frame.setVisible(true);
+	}
+}
